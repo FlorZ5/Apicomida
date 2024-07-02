74dd671d19d497fbcc24beeefac8ee1953af071e
Astridrdzza03 <21360000607@utna.edu.mx>
2024-07-01 23:08:02 -0600

    rendee

--- database.js
+++ database.js
@@ -1,12 +1,15 @@
-import mongoose, { Mongoose } from "mongoose";
-const uri = "mongodb://127.0.0.1:27017/comida"; 
-//Creamos la conexión
-const connectDB=async()=>{
+import mongoose from 'mongoose';
+
+const uri = ""; // URI de conexión a MongoDB Atlas
+
+// Creamos la conexión
+const connectDB = async () => {
   try {
     const db = await mongoose.connect(uri);
-    console.log('Base de datos conectada: ',db.connection.name);
+    console.log('Base de datos conectada:', db.connection.name);
   } catch (error) {
-    console.log('Error:',error.message);
+    console.error('Error de conexión a la base de datos:', error.message);
+    process.exit(1); // Termina la ejecución del proceso en caso de error de conexión
   }
 }
 
